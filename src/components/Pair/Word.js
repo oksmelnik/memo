d6f8e11886a3feb1e691f3bca04e3bc09cf304f3
Oksana Melnik <oksana@dedact.nl>
2019-12-26 09:19:11 +0100

    word with gaps

--- src/components/Pair/Word.js
+++ src/components/Pair/Word.js
@@ -1,24 +1,80 @@
 import React from 'react'
 import { TextareaAutosize } from '@material-ui/core';
+import Gap from './Gap'
 import './Pair.css'
 
 function Word(props) {
+
+  const value = props.pair[props.order]
+
+  const showEditFiels = () => {
+    if (!props.editMode) {
+      return false
+    } else if (props.type === 'gap' && props.order === 'right') {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  const showOriginText = () => {
+    if (!props.editMode && props.type !== 'gap') {
+      return true
+    }
+  }
+
+  const getGaps = () => {
+    if (!props.type === 'gap') {
+      return
+    }
+
+    const gapWords = props.order === 'left' && props.pair.gap ?
+      props.pair.gap.words.map((item, index) => {
+        if (props.pair.gap.selected.includes(index)) {
+          return '...'
+        } else {
+          return item
+        }
+      }) :
+      props.pair.gap.selected.map(index => props.pair.gap.words[index])
+
+    if (gapWords) {
+      return gapWords.map((word, index) => {
+        return (
+          <Gap
+            key={index}
+            value={word}
+            selectGap={(e) => props.selectGap(e, props.pair.id, index, value)}
+          />
+        )
+      })
+    }
+}
 
   return (
     <div
       className="word"
     >
-      {!props.editMode &&
-        <p onClick={props.toggleEdit}>{props.value}</p>
+      {showOriginText() &&
+        <p onClick={props.setEdit}>{props.value}</p>
       }
-      {props.editMode &&
-        <TextareaAutosize
-        aria-label="empty textarea"
-        placeholder="Empty"
-        defaultValue={props.value}
-        onChange={props.onChange}/>
-      }
+
+      <div className='className="word-edit"'>
+        {showEditFiels() &&
+          <div>
+          <TextareaAutosize
+          aria-label="empty textarea"
+          placeholder="Empty"
+          defaultValue={value}
+          onChange={props.onChange}/>
+          </div>
+        }
+
+        <div className="splited">{getGaps()}</div>
+
+      </div>
     </div>
   )
+
 }
 export default Word;
